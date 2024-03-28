210e5c907dc93b471acd187a5232e78f398cd725
Ekrigh <Erik.krigh@hotmail.com>
2024-03-28 10:25:12 +0100

    City Entitet

---
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Entitys/City.java
@@ -0,0 +1,69 @@
+package org.dreamteam.wigellsmoviesstore.Entitys;
+
+import jakarta.persistence.*;
+
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "city")
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "city_id")
+    private int id;
+
+    @Column(name = "city", length = 50)
+    private String name;
+
+    @ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST })
+    @JoinColumn(name = "country_id")
+    private Country country;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+
+
+    public City() {
+
+    }
+    public City(int id, String name, Country country, Timestamp lastUpdate) {
+        this.id = id;
+        this.name = name;
+        this.country = country;
+        this.lastUpdate = lastUpdate;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int cityId) {
+        this.id = cityId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String cityName) {
+        this.name = cityName;
+    }
+
+    public Country getCountry() {
+        return country;
+    }
+
+    public void setCountry(Country country) {
+        this.country = country;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+}
