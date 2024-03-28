4346099d8449dcb2f95568bece15e87d84c17d2f
Anisocoria84 <anna.ekerot@gmail.com>
2024-03-28 13:34:40 +0100

    address

---
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Entitys/Address.java
@@ -0,0 +1,142 @@
+package org.dreamteam.wigellsmoviesstore.Entitys;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.Type;
+import org.hibernate.engine.internal.Cascade;
+
+import java.sql.Timestamp;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "address")
+public class Address {
+
+    @Id
+
+    @GeneratedValue
+    @Column(name = "address_id")
+    private int id;
+
+    @Column(name = "address", length = 50)
+    private String address;
+
+    @Column(name = "address2", length = 50)
+    private String address2;
+
+    @Column(name = "district", length = 20)
+    private String district;
+
+    @Column(name = "city_id")
+    private int cityId;
+
+    @Column(name = "postal_code", length = 10)
+    private String postalCode;
+
+    @Column(name = "phone", length = 20)
+    private String phone;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    @ManyToOne(cascade = { CascadeType.MERGE, CascadeType.PERSIST})
+    @JoinColumn(name = "city_id")
+    private City city;
+
+    public Address(){
+
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        id = id;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
+    }
+
+    public String getAddress2() {
+        return address2;
+    }
+
+    public void setAddress2(String address2) {
+        this.address2 = address2;
+    }
+
+    public String getDistrict() {
+        return district;
+    }
+
+    public void setDistrict(String district) {
+        this.district = district;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+    public String getPostalCode() {
+        return postalCode;
+    }
+
+    public void setPostalCode(String postalCode) {
+        this.postalCode = postalCode;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public List<Customer> getCustomers() { return customers;}
+
+    public void setCustomers(List<Customer> customers) {this.customers = customers;}
+    public List<Store> getStores() {
+        return stores;
+    }
+
+    public void setStores(List<Store> stores) {
+        this.stores = stores;
+    }
+
+    public List<Staff> getStaffs() {
+        return staffs;
+    }
+
+    public void setStaffs(List<Staff> staffs) {
+        this.staffs = staffs;
+    }
+
+    public City getCity() {
+        return city;
+    }
+
+    public void setCity(City city) {
+        this.city = city;
+    }
+
+}
