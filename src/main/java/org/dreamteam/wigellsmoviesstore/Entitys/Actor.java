88d8e3ca25fce2cd937d57b94936b2768c732a8a
Markus Göras <m.goras@hotmail.com>
2024-03-28 09:18:29 +0100

    Create Actor

---
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Entitys/Actor.java
@@ -0,0 +1,89 @@
+package org.dreamteam.wigellsmoviesstore.Entitys;
+
+import jakarta.persistence.*;
+import org.hibernate.annotations.UpdateTimestamp;
+
+import java.sql.Timestamp;
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "actor")
+public class Actor {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "actor_id")
+    private int Id;
+
+    @Column(name = "first_name", length = 45)
+    private String firstName;
+
+    @Column(name = "last_name", length = 45)
+    private String lastName;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "film_actor",
+    joinColumns = {@JoinColumn(name = "actor_id")},
+    inverseJoinColumns = {@JoinColumn(name = "film_id")})
+    private List<Film> films = new ArrayList<>();
+
+    // Constructor
+    public Actor() {
+    }
+
+    // Getters & Setters
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public LocalDateTime getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(LocalDateTime lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
+
+    public void addFilm(Film film) {
+        this.films.add(film);
+    }
+
+    public void removeFilmFromList(Film film){
+        this.films.remove(film);
+    }
+
+}
