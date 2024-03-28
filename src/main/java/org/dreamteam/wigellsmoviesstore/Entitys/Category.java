14a4c94df5cfb32eaa1d71b20b9a655295451814
thyrgraw <153826964+thyrgraw@users.noreply.github.com>
2024-03-28 10:58:32 +0100

    Skapat Category

---
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Entitys/Category.java
@@ -0,0 +1,62 @@
+package org.dreamteam.wigellsmoviesstore.Entitys;
+import jakarta.persistence.*;
+
+import java.util.ArrayList;
+import java.sql.Timestamp;
+
+@Entity
+@Table(name = "category")
+public class Category {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "category_id")
+    private int Id;
+
+    @Column(name = "name", length = 25)
+    private String name;
+
+    @Column(name = "last_update")
+    private Timestamp lastUpdate;
+
+    @ManyToMany(fetch = FetchType.EAGER)
+    @JoinTable(name = "film_category",
+            joinColumns = {@JoinColumn(name = "film_id")},
+            inverseJoinColumns = {@JoinColumn(name = "category_id")})
+    private List<Film> filmList = new ArrayList<>();
+
+    public Category() {
+    }
+
+    public int getId() {
+        return Id;
+    }
+
+    public void setId(int id) {
+        Id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Timestamp getLastUpdate() {
+        return lastUpdate;
+    }
+
+    public void setLastUpdate(Timestamp lastUpdate) {
+        this.lastUpdate = lastUpdate;
+    }
+
+    public List<Film> getFilmList() {
+        return filmList;
+    }
+
+    public void setFilmList(List<Film> filmList) {
+        this.filmList = filmList;
+    }
+}
