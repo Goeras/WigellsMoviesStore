b1b073ce140ece3cd64d78ea638fd0b0d39fbe6e
SJEkstrom <162336664+SJEkstrom@users.noreply.github.com>
2024-04-03 12:19:20 +0200

    posview

--- src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/PosController.java
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/PosController.java
@@ -1,7 +1,10 @@
 package org.dreamteam.wigellsmoviesstore.Controllers;
 
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.HBox;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -10,6 +13,14 @@ public class PosController {
     @FXML
     private Label topLabel;
     ViewManager viewManager;
+    @FXML
+    private HBox searchCustomer;
+    @FXML
+    private HBox confirmCustomer;
+    @FXML
+    private HBox chosenCustInfo;
+    @FXML
+    private Button backButton;
 
     public void initialize(){
         viewManager = new ViewManager();
@@ -17,5 +28,23 @@ public class PosController {
     @FXML
     protected void onBackButtonClick() throws IOException {
         viewManager.showMenuView((Stage) topLabel.getScene().getWindow());
+    }
+    @FXML
+    private void onConfirmCustomerButton(){
+        chosenCustInfo.setManaged(true);
+        chosenCustInfo.setVisible(true);
+        searchCustomer.setManaged(false);
+        searchCustomer.setVisible(false);
+        confirmCustomer.setManaged(false);
+        confirmCustomer.setVisible(false);
+    }
+    @FXML
+    private void onChangeCustomerButton(){
+        searchCustomer.setManaged(true);
+        searchCustomer.setVisible(true);
+        confirmCustomer.setManaged(true);
+        confirmCustomer.setVisible(true);
+        chosenCustInfo.setManaged(false);
+        chosenCustInfo.setVisible(false);
     }
 }
