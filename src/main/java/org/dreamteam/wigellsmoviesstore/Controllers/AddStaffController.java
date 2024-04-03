2c6c331e04cf6c2654c7955c79de48172d1c6697
SJEkstrom <162336664+SJEkstrom@users.noreply.github.com>
2024-04-03 10:47:39 +0200

    uppladdning bild addstaff

--- src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/AddStaffController.java
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/AddStaffController.java
@@ -2,14 +2,24 @@ package org.dreamteam.wigellsmoviesstore.Controllers;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
+import java.io.File;
+import java.io.FileInputStream;
 import java.io.IOException;
+import java.io.InputStream;
 
 public class AddStaffController {
     private ViewManager viewManager;
     @FXML
     private Label topLabel;
+    @FXML
+    private ImageView imageView;
+    @FXML
+    private Image image;
 
     public void initialize(){
         viewManager = new ViewManager();
@@ -18,5 +28,25 @@ public class AddStaffController {
     private void onBackButtonClick() throws IOException {
         viewManager.showStaffView((Stage) topLabel.getScene().getWindow());
     }
+    @FXML
+    private void onUploadPictureButtonClick(){
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Välj bild");
+        fileChooser.getExtensionFilters().addAll(new FileChooser.ExtensionFilter("Bilder", "*.jpg", "*.png", "*.jpeg"));
+        File selectedFile = fileChooser.showOpenDialog(null);
 
+        if(selectedFile!=null){
+            try{
+                InputStream inputStream = new FileInputStream(selectedFile);
+                image = new Image(inputStream);
+            }
+            catch (Exception e){
+                e.printStackTrace();
+                System.out.println("Fel vid inläsning av bild");
+            }
+        }
+        imageView.setFitWidth(60);
+        imageView.setFitHeight(70);
+        imageView.setImage(image);
+    }
 }
