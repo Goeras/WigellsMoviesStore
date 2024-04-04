182c0ae0a351a8deaa3e997761fb068182823b10
thyrgraw <153826964+thyrgraw@users.noreply.github.com>
2024-04-04 11:49:52 +0200

    updte på customer add, manager och view.fxlm

--- src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/AddCustomerController.java
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/AddCustomerController.java
@@ -1,22 +1,79 @@
 package org.dreamteam.wigellsmoviesstore.Controllers;
 import javafx.fxml.FXML;
+import javafx.scene.control.CheckBox;
+import javafx.scene.control.ChoiceBox;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.dreamteam.wigellsmoviesstore.Entitys.Country;
+import org.dreamteam.wigellsmoviesstore.IoValidator;
+import org.dreamteam.wigellsmoviesstore.Managers.CustomerManager;
 import org.dreamteam.wigellsmoviesstore.Managers.ViewManager;
 
 import java.io.IOException;
+import java.util.List;
 
 public class AddCustomerController {
     @FXML
     private Label topLabel;
+
+    @FXML
+    private TextField firstName;
+    @FXML
+    private TextField lastName;
+    @FXML
+    private TextField email;
+    @FXML
+    private TextField phone;
+    @FXML
+    private TextField address1;
+    @FXML
+    private TextField address2;
+    @FXML
+    private TextField district;
+    @FXML
+    private TextField postalCode;
+    @FXML
+    private TextField city;
+    @FXML
+    private ChoiceBox<Country> country;
+
     private ViewManager viewManager;
+    private CustomerManager customerManager;
+    private IoValidator ioValidator;
 
     public void initialize(){
-        viewManager = new ViewManager();
+        //viewManager = new ViewManager();
+        List<Country> countries = customerManager.getCountryList();
+        country.getItems().addAll(countries);
+
+
+
     }
     @FXML
     private void onBackButtonClick() throws IOException {
         viewManager.showCustomerView((Stage) topLabel.getScene().getWindow());
+    }
+    @FXML
+    public void onSaveButtonClick() throws IOException{
+
+
+        // valideringar från iovalidator innan något läggs in.
+        boolean validName = ioValidator.validateStringNotEmpty(firstName.getText());
+        boolean validLastName = ioValidator.validateStringNotEmpty(lastName.getText());
+        boolean validEmail = ioValidator.validateEmail(email.getText());
+        boolean validPhoneNum = ioValidator.validateInteger(phone.getText());
+        boolean validAddess = ioValidator.validateStringNotEmpty(address1.getText());
+        boolean validDistrict = ioValidator.validateStringNotEmpty(district.getText());
+        boolean validPostCode = ioValidator.validatePostNum(postalCode.getText());
+        boolean validCity = ioValidator.validateStringNotEmpty(city.getText());
+
+        if(validName && validLastName && validEmail && validPhoneNum && validAddess && validDistrict && validPostCode && validCity){
+            customerManager.newCustomer(firstName.getText(),lastName.getText(),email.getText(),phone.getText(),address1.getText(),address2.getText(),district.getText(),postalCode.getText(),city.getText(),country.toString());
+        } else {
+            //felmedelande?
+        }
+
     }
 
 
--- src/main/java/org/dreamteam/wigellsmoviesstore/Managers/CustomerManager.java
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Managers/CustomerManager.java
@@ -1,4 +1,78 @@
 package org.dreamteam.wigellsmoviesstore.Managers;
 
+import org.dreamteam.wigellsmoviesstore.DAO.CountryDAO;
+import org.dreamteam.wigellsmoviesstore.Entitys.Address;
+import org.dreamteam.wigellsmoviesstore.Entitys.City;
+import org.dreamteam.wigellsmoviesstore.Entitys.Country;
+import org.dreamteam.wigellsmoviesstore.Entitys.Customer;
+
+import java.sql.Date;
+import java.sql.Timestamp;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
 public class CustomerManager {
+    Customer newCustomer = new Customer();
+    Address newAddress = new Address();
+    City newCity = new City();
+    Country newCountry = new Country();
+    CountryDAO countryDAO = new CountryDAO();
+
+    public void newCustomer(String firstName, String lastName, String email, String phone, String address1, String address2, String district, String postalcode, String city, String country){
+
+
+        newCustomer.setFirstName(firstName);
+        newCustomer.setLastName(lastName);
+        newCustomer.setEmail(email);
+        newCustomer.setActive(true);
+        newCustomer.setCreateDate(new Date(System.currentTimeMillis()));
+        newCustomer.setLast_update(Timestamp.valueOf(LocalDateTime.now()));
+
+        newAddress.setAddress(address1);
+        newAddress.setAddress(address2);
+        newAddress.setDistrict(district);
+        newAddress.setPostalCode(postalcode);
+        newAddress.setPhone(phone);
+        //newAddress.setLocation();
+        newAddress.setLastUpdate(Timestamp.valueOf(LocalDateTime.now()));
+
+        //Gör en metod som söker på land och hämtar country id för att lägga till i en ny stad som läggs in.
+        int countryId = getCountryId(country);
+        newCity.setCountry(countryId);
+        newCity.setName(city);
+        newCity.setLastUpdate(Timestamp.valueOf(LocalDateTime.now()));
+
+        //bör länderna hämtas och visas som en lista?
+        //lägga till en stad med country id?
+
+        //newCity.setCountry();
+
+        newCountry.setName(country);
+        newCountry.setLastUpdate(Timestamp.valueOf(LocalDateTime.now()));
+
+
+    }
+    public List<Country> getCountryList(){
+
+        List<Country> countryList = countryDAO.getAllCountries();
+
+        return countryList;
+    }
+    public int getCountryId(String country){
+
+        List<Country> countryList = countryDAO.getAllCountries();
+
+        for (Country currentCountry : countryList) {
+            if (currentCountry.getName().equalsIgnoreCase(country)) {
+                return currentCountry.getId();
+            }
+        }
+        // Returnera -1 om landet inte hittades
+        return -1;
+    }
+    public void citySearch(){
+        //metod som gör en query efter land och skickar tillbaks country id?
+    }
 }
