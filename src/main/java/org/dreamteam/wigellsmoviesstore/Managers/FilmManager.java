56b5cd8374a7a3c6cb0869d0c7aea169354b9bda
Ekrigh <Erik.krigh@hotmail.com>
2024-04-04 11:36:01 +0200

    FilmManager + addFilm

---
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Managers/FilmManager.java
@@ -0,0 +1,28 @@
+package org.dreamteam.wigellsmoviesstore.Managers;
+
+import org.dreamteam.wigellsmoviesstore.DAO.DAOmanager;
+import org.dreamteam.wigellsmoviesstore.Entitys.Film;
+import org.dreamteam.wigellsmoviesstore.IoConverter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class FilmManager {
+  DAOmanager daoManager = new DAOmanager();
+
+  public void addFilm(List<String> infoList) {
+    Film film = new Film();
+    film.setTitle(infoList.get(0));
+    film.setDescription(infoList.get(1));
+    film.setReleaseYear(IoConverter.stringToShort(infoList.get(2)));
+    film.setLanguage(daoManager.getLanguageDAO().readLanguage(IoConverter.stringToInteger(infoList.get(3))));
+    film.setOriginalLanguage(daoManager.getLanguageDAO().readLanguage(IoConverter.stringToInteger(infoList.get(4))));
+    film.setRentalDuration(IoConverter.stringToByte(infoList.get(5)));
+    film.setLength(IoConverter.stringToShort(infoList.get(6)));
+    film.setReplacementCost(IoConverter.stringToDouble(infoList.get(7)));
+    film.setRentalRate(IoConverter.stringToDouble(infoList.get(8)));
+    film.setSpecialFeatures(infoList.get(9));
+
+
+  }
+}
