ae8c2ca545e891d28ce3c624089ef448dccfc3d3
SJEkstrom <162336664+SJEkstrom@users.noreply.github.com>
2024-04-04 15:50:37 +0200

    customerView

--- src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/CustomerViewController.java
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Controllers/CustomerViewController.java
@@ -1,19 +1,54 @@
 package org.dreamteam.wigellsmoviesstore.Controllers;
 
+import javafx.collections.ObservableList;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
 import javafx.stage.Stage;
+import org.dreamteam.wigellsmoviesstore.Entitys.Rental;
+import org.dreamteam.wigellsmoviesstore.Managers.CustomerManager;
 import org.dreamteam.wigellsmoviesstore.Managers.ViewManager;
 
 import java.io.IOException;
+import java.lang.reflect.Array;
 
 public class CustomerViewController {
 @FXML
     private Label topLabel;
 ViewManager viewManager;
+CustomerManager customerManager;
+@FXML
+private TextField searchCustomer;
+@FXML
+private Label firstName;
+@FXML
+private Label lastName;
+@FXML
+private Label email;
+@FXML
+private Label active;
+@FXML
+private Label createDate;
+@FXML
+private Label address1;
+@FXML
+private Label address2;
+    @FXML
+    private Label disctrict;
+    @FXML
+    private Label postalCode;
+    @FXML
+    private Label city;
+    @FXML
+    private Label country;
+    ObservableList<Rental> rentalHistory;
+
 
 public void initialize(){
+
     viewManager = new ViewManager();
+    customerManager = new CustomerManager();
 }
 @FXML
 private void onBackButtonClick() throws IOException {
@@ -27,4 +62,21 @@ private void onBackButtonClick() throws IOException {
     private void onUpdateCustomerClick() throws IOException {
     viewManager.showUpdateCustomerView((Stage) topLabel.getScene().getWindow());
 }
+@FXML
+    private void onSearchButtonClick(){
+        String customerId = searchCustomer.getText();
+        String[] info = customerManager.getCustomerInfo(customerId);
+        firstName.setText(info[0]);
+        lastName.setText(info[1]);
+        email.setText(info[2]);
+        active.setText(info[3]);
+        createDate.setText(info[4]);
+        address1.setText(info[5]);
+        address2.setText(info[6]);
+        disctrict.setText(info[7]);
+        city.setText(info[8]);
+        postalCode.setText(info[9]);
+        country.setText(info[10]);
+}
+
 }
--- src/main/java/org/dreamteam/wigellsmoviesstore/Managers/CustomerManager.java
+++ src/main/java/org/dreamteam/wigellsmoviesstore/Managers/CustomerManager.java
@@ -1,9 +1,12 @@
 package org.dreamteam.wigellsmoviesstore.Managers;
 
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import org.dreamteam.wigellsmoviesstore.CurrentStore;
 import org.dreamteam.wigellsmoviesstore.DAO.CountryDAO;
 import org.dreamteam.wigellsmoviesstore.DAO.DAOmanager;
 import org.dreamteam.wigellsmoviesstore.Entitys.*;
+import org.dreamteam.wigellsmoviesstore.IoConverter;
 
 import java.sql.Date;
 import java.sql.Timestamp;
@@ -82,6 +85,29 @@ public class CustomerManager {
             }
         }
         return true;
+    }
+    public String[] getCustomerInfo(String custId){
+        int customerId = IoConverter.stringToInteger(custId);
+        String[] info = new String[11];
+        Customer customer = daoManager.getCustomerDAO().readCustomer(customerId);
+        info[0] = customer.getFirstName();
+        info[1] = customer.getLastName();
+        info[2] = customer.getEmail();
+        info[3] = Boolean.toString(customer.getActive());
+        info[4] = customer.getCreateDate().toString();
+        info[5] = customer.getAdress().getAddress();
+        info[6] = customer.getAdress().getAddress2();
+        info[7] = customer.getAdress().getDistrict();
+        info[8] = customer.getAdress().getPostalCode();
+        info[9] = customer.getAdress().getCity().getName();
+        info[10] =customer.getAdress().getCity().getCountry().getName();
+        return info;
+    }
+    public ObservableList<Rental> getCustomerRentals(String custId){
+        int customerId = IoConverter.stringToInteger(custId);
+        Customer customer = daoManager.getCustomerDAO().readCustomer(customerId);
+        ObservableList<Rental> rentals = FXCollections.observableList(customer.getRentals());
+        return rentals;
     }
 
 }
